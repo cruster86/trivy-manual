1f027e5033d9630ec4c365566995a1952776ddac
cruster86 <62769621+cruster86@users.noreply.github.com>
2024-05-06 19:03:46 +0300

    Update trivy.sh

--- trivy.sh
+++ trivy.sh
@@ -4,9 +4,9 @@ export DATE=$(date '+%Y-%m-%d')
 
 while read LIST ; do
 
-# docker run --rm -e TRIVY_USERNAME=zerodistance -e TRIVY_PASSWORD=taydrgJqzPPtLTnGvwpX aquasec/trivy --timeout 60m image --scanners vuln --severity HIGH,CRITICAL $LIST
+# docker run --rm -e TRIVY_USERNAME=user_name -e TRIVY_PASSWORD=user_passwd aquasec/trivy --timeout 60m image --scanners vuln --severity HIGH,CRITICAL $LIST
 # docker run --rm  aquasec/trivy -q image --scanners vuln --severity HIGH,CRITICAL $LIST
-# docker run --rm -e TRIVY_USERNAME=zerodistance -e TRIVY_PASSWORD=taydrgJqzPPtLTnGvwpX --mount src="$(pwd)/trivycache",target=/root/trivycache,type=bind aquasec/trivy --timeout 60m --cache-dir /root/trivycache/ image --scanners vuln --severity HIGH,CRITICAL --no-progress $LIST
+# docker run --rm -e TRIVY_USERNAME=user_name -e TRIVY_PASSWORD=user_passwd --mount src="$(pwd)/trivycache",target=/root/trivycache,type=bind aquasec/trivy --timeout 60m --cache-dir /root/trivycache/ image --scanners vuln --severity HIGH,CRITICAL --no-progress $LIST
 
 #trivy \
 #  --cache-dir trivycache/ \
